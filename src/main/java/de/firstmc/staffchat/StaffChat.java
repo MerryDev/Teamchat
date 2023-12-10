bd77a4899dbab55b9fc6445a92bfb48cc5720c83
Merry <svgr1808@outlook.de>
2023-12-10 22:47:23 +0100

    Modify import statement in StaffChat.java
    Changed the import statement from "com.google.common.eventbus.Subscribe" to "com.velocitypowered.api.event.Subscribe". This switch improves the ability to interact with Velocity's API, enhancing the initialization process of StaffChat and communication between commands.

--- src/main/java/de/firstmc/staffchat/StaffChat.java
+++ src/main/java/de/firstmc/staffchat/StaffChat.java
@@ -1,9 +1,9 @@
 package de.firstmc.staffchat;
 
-import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
 import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandMeta;
+import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
