1cc46c6a69be32597e110030a8556f1ad09b9cd9
Riccardo <76207588+ExceptionThread@users.noreply.github.com>
2023-12-10 21:44:02 +0100

    Main File (#2)

--- src/main/java/de/firstmc/staffchat/StaffChat.java
+++ src/main/java/de/firstmc/staffchat/StaffChat.java
@@ -1,4 +1,28 @@
 package de.firstmc.staffchat;
 
-public class StaffChat {
+import com.google.common.eventbus.Subscribe;
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+
+@Plugin(id = "TeamChat", name = "TeamChat", version = "0.1.0-SNAPSHOT",
+        url = "https://firstmc.de", description = "Velocity TeamChat", authors = {"MerryChristmas,ExceptionThread"})
+public record StaffChat(ProxyServer proxyServer) {
+    private static StaffChat instance;
+
+    @Inject
+    public StaffChat {
+        instance = this;
+    }
+
+    @Subscribe
+    public void handle(ProxyInitializeEvent event) {
+        this.proxyServer.getCommandManager().register("tc", new TeamChatCommand());
+        this.proxyServer.getCommandManager().register("tc", new StaffChatCommand());
+    }
+
+    public static StaffChat getInstance() {
+        return instance;
+    }
 }
