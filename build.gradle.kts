056b1fe97e2d35db32373626f05cfc6d1c8a10a2
Merry <svgr1808@outlook.de>
2023-12-10 21:59:39 +0100

    Remove JUnit dependencies from build.gradle.kts
    JUnit dependencies and the associated test task statement have been removed from the build.gradle.kts file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,4 @@ dependencies {
 
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.3")
-
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
