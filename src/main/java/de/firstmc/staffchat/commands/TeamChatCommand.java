8cb6ffd124867023700442676550b701c35a48f5
Merry <svgr1808@outlook.de>
2023-12-10 23:10:56 +0100

    Update TeamChatCommand to SimpleCommand
    Updated TeamChatCommand from implementing RawCommand to SimpleCommand and revised command execution logic. Now, it verifies not only if the source is a player but also checks for necessary permissions, guiding users more effectively. Moreover, it also checks if there are arguments in the command, making the team chat command more robust and user-friendly.

--- src/main/java/de/firstmc/staffchat/commands/TeamChatCommand.java
+++ src/main/java/de/firstmc/staffchat/commands/TeamChatCommand.java
@@ -1,42 +1,49 @@
 package de.firstmc.staffchat.commands;
 
 import com.velocitypowered.api.command.CommandSource;
-import com.velocitypowered.api.command.RawCommand;
+import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import lombok.AllArgsConstructor;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @AllArgsConstructor
-public final class TeamChatCommand implements RawCommand {
+public final class TeamChatCommand implements SimpleCommand {
 
 	private final ProxyServer server;
 
 	@Override
 	public void execute(final Invocation invocation) {
 		CommandSource source = invocation.source();
+		String[] args = invocation.arguments();
 
-		if (!(source instanceof Player)) {
-			source.sendMessage(Component.text("To do this, you have to be a Player.", NamedTextColor.RED));
+		if (!(source instanceof Player player)) {
+			source.sendMessage(Component.text("Do do this, you have to be a player.", NamedTextColor.RED));
 			return;
 		}
 
-		for (Player player : this.server.getAllPlayers()) {
-			if (!player.hasPermission("Teamchat.FirstMC")) continue;
-			player.sendMessage(MiniMessage.miniMessage().deserialize(invocation.arguments(), TagResolver.standard())); // Allow the player to use tags to colorize the message
+		if (!player.hasPermission("Teamchat.FirstMC")) {
+			player.sendMessage(Component.text("You don't have permission to use this command.", NamedTextColor.RED));
+			return;
 		}
-	}
 
-	@Override
-	public boolean hasPermission(final Invocation invocation) {
-		return invocation.source().hasPermission("Teamchat.FirstMC");
+		if (args.length == 0) {
+			player.sendMessage(Component.text("Please use /teamchat <Message>!", NamedTextColor.RED));
+			return;
+		}
+
+		String message = String.join(" ", args);
+    	server.getAllPlayers().forEach(p -> {
+    		if (p.hasPermission("Teamchat.FirstMC")) {
+    			p.sendMessage(Component.text("[TeamChat] " + player.getUsername() + ": " + message, NamedTextColor.GRAY));
+    		}
+    	});
+
 	}
 
 	@Override
